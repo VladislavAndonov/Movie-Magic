aac95e0012b08b97fee07b812dae81d18e794a65
Vladislav Andonov <wladoandonov@gmail.com>
2024-10-31 11:23:38 +0200

    Render home page

--- src/index.js
+++ src/index.js
@@ -12,8 +12,10 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+app.use(express.static('public'));
+
 app.get("/", (req, res) => {
-    res.render("index");
+    res.render("home");
 });
 
 app.listen(5000, () =>
