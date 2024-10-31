844aecfcb5a7280cb660c02bdaadf3ee3deb7954
Vladislav Andonov <wladoandonov@gmail.com>
2024-10-31 11:46:36 +0200

    Add home contoller

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ JS Back-End Course Workshop @SoftUni
    2. Setup express
    3. Setup handlebars
    4. Add resources
+   5. Setup static route
+   6. Render home page
+   7. Add routes and controllers
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import express from "express";
 import handlebars from "express-handlebars";
 
+import homeController from "./controllers/homeController.js";
+
 const app = express();
 
 app.engine(
@@ -12,11 +14,9 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("home");
-});
+app.use(homeController);
 
 app.listen(5000, () =>
     console.log("Server is listening on http://localhost:5000...")
