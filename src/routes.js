73a6f86654aeeab2159e0dc5088130ed25b20f47
Vladislav Andonov <wladoandonov@gmail.com>
2024-11-02 18:26:34 +0200

    Dynamic rendering movies

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
 import homeController from "./controllers/homeController.js";
+import createController from "./controllers/createController.js";
 
 const router = Router();
 
 router.use(homeController);
+router.use(createController);
 
 export default router;
