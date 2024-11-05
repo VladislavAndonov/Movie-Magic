b5d7745d19a65d6f9d3bdd84959db0fe87b69388
Vladislav Andonov <wladoandonov@gmail.com>
2024-11-05 12:22:31 +0200

    Add body parser middleware

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import express from "express";
 import handlebars from "express-handlebars";
 
-import routes from './routes.js';
+import routes from "./routes.js";
 
 const app = express();
 
@@ -14,6 +14,7 @@ app.engine(
 app.set("view engine", "hbs");
 app.set("views", "src/views");
 
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static("public"));
 
 app.use(routes);
