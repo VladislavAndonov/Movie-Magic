f7bcc0a184690e325b3ab1941c42873dbade69a5
Vladislav Andonov <wladoandonov@gmail.com>
2024-11-09 19:53:00 +0200

    Show casts in details page

--- src/models/Movie.js
+++ src/models/Movie.js
@@ -1,16 +1,21 @@
-import { Schema, model } from "mongoose";
+import { Schema, model, Types } from "mongoose";
 
 const movieSchema = new Schema({
-    
     title: String,
     genre: String,
     director: String,
     year: Number,
     rating: Number,
     description: String,
-    imageUrl: String
+    imageUrl: String,
+    casts: [
+        {
+            type: Types.ObjectId,
+            ref: 'Cast'
+        },
+    ],
 });
 
-const Movie = model('Movie', movieSchema);
+const Movie = model("Movie", movieSchema);
 
 export default Movie;
--- src/services/castService.js
+++ src/services/castService.js
@@ -2,7 +2,7 @@ import Cast from "../models/Cast.js";
 
 const create = (cast) => Cast.create(cast);
 
-const getAll = () => Cast.find();
+const getAll = (filter) => Cast.find(filter);
 
 export default {
     create,
