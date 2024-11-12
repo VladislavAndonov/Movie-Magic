9717b990059275e12f48a1f0503c526496242187
Vladislav Andonov <wladoandonov@gmail.com>
2024-11-12 22:22:40 +0200

    Basic user creation in db

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,9 +1,21 @@
-import { Router } from 'express';
+import { Router } from "express";
+import authService from "../services/authService.js"
 
 const router = Router();
 
-router.get('/register', (req, res) => {
-    res.render('auth/register');
+router.get("/register", (req, res) => {
+    res.render("auth/register");
+});
+
+router.post("/register", async (req, res) => {
+    const { email, password, rePassword } = req.body;
+
+
+    console.log(email, password, rePassword);
+    
+    await authService.register(email, password);
+
+    res.redirect("/");
 });
 
 export default router;
