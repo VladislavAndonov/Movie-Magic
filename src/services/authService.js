5f42c635c8bce306bb52901513164cb013159f45
Vladislav Andonov <wladoandonov@gmail.com>
2024-11-14 18:38:51 +0200

    Generate jwt token on successful login

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,9 @@
-import User from "../models/User.js";
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
+import User from "../models/User.js";
+
+const SECRET = "f30ra7aea4846c5d7ea735e358bwtc35";
 const register = (email, password) => {
     // TODO: Check if email already exists
 
@@ -15,8 +18,18 @@ const login = async (email, password) => {
         throw new Error("User does not exist!");
     }
 
-    // TODO: Verify if the entered password match the hashed pass in DB
+    // TODO: Validate if the entered password match the hashed pass in DB
+    const isValid = await bcrypt.compare(password, user.password);
+
+    if (!isValid) {
+        throw new Error("Password does not match");
+    }
+
     // TODO: Generate and return JWT token
+    const payload = { _id: user._id, email };
+    const token = jwt.sign(payload, SECRET, { expiresIn: "2h" });
+    
+    return token;
 };
 
 export default {
